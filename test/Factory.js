2daa9398763e424d018e720843e6b3412507912d
Meettank3 <125813530+Meettank3@users.noreply.github.com>
2025-09-13 02:26:26 +0530

    Done Test of Factory till 1.01.13

--- test/Factory.js
+++ test/Factory.js
@@ -3,5 +3,48 @@ const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
 describe("Factory", function () {
+    const Fee = ethers.parseUnits("0.01",18); // 0.01 ether in wei
+
+
+    // this function deploy the contract and returns the contract instance as factory
+    async function deployFactoryFixture() {
+        // fetch Account's
+        const [deployer] = await ethers.getSigners();
+        
+        // fetch the contract
+        const Factory = await ethers.getContractFactory("Factory");
+        // deploy the contract
+        const factory = await Factory.deploy(Fee);
+        return { factory, deployer }; //this line is to export variables
+    }   //to use its return var we must use line below
+        // const { factory } = await loadFixture(deployFactoryFixture)
+
+    describe("Deployment", function() {
+        it("Should have immutabe value", async() => {
+            const { factory } = await loadFixture(deployFactoryFixture);
+            expect(await factory.fee()).to.equal(Fee);              
+        });
+
+        it("Should have Same Owner", async() => {
+            const { factory, deployer } = await loadFixture(deployFactoryFixture);
+            expect(await factory.owner()).to.equal(deployer.address.toString());
+        });
+    });
 
 })
+
+
+// Basic tst 
+/*
+it("Should have a name", async () => {
+    // fetch the contract
+    const Factory = await ethers.getContractFactory("Factory");
+    // deploy the contract
+    const factory = await Factory.deploy();
+    //check name
+    const name = await factory.name();
+    console.log("Name is:",name);
+    expect(name).to.equal("Factory");
+});
+*/
+
