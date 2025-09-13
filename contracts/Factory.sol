9982ab4b257036ab44c9574266b8f15a1dea92dc
Meettank3 <125813530+Meettank3@users.noreply.github.com>
2025-09-13 18:27:11 +0530

    1.21.45

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,13 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.27;
 
+import {Token} from "./Token.sol";
+
 contract Factory {
 
     uint256 public immutable fee;
     address public owner;
 
+    uint256 public totalTokens;
+    address[] public tokens;
+
     constructor(uint256 _fee) {
         fee = _fee;
         owner = msg.sender;
     }
+
+    function create(
+        string memory _name,
+        string memory _symbole
+    ) external payable{
+        // create a new token which require 2 thing from erc20 which is name and symbole by calling another contract Token
+        Token token = new Token( msg.sender, _name, _symbole, 1_000_000 ether );
+        // then Save the Token on BLOCKCHAIN
+        tokens.push(address(token));
+        
+        totalTokens++;
+
+        // List token for sale
+        // tell people its Live
+    }
+
 }
--- test/Factory.js
+++ test/Factory.js
@@ -5,19 +5,25 @@ const { ethers } = require("hardhat")
 describe("Factory", function () {
     const Fee = ethers.parseUnits("0.01",18); // 0.01 ether in wei
 
-
     // this function deploy the contract and returns the contract instance as factory
     async function deployFactoryFixture() {
         // fetch Account's
-        const [deployer] = await ethers.getSigners();
-        
+        const [deployer, creator] = await ethers.getSigners();        
         // fetch the contract
         const Factory = await ethers.getContractFactory("Factory");
         // deploy the contract
         const factory = await Factory.deploy(Fee);
-        return { factory, deployer }; //this line is to export variables
+        // create token
+        const transaction = await factory.connect(creator).create("Dapp Uni","DAP", { value: Fee });
+        await transaction.wait();
+        // get Token Address
+
+        const tokenAddress = await factory.tokens(0);
+        const token = await ethers.getContractAt("Token", tokenAddress);
+
+        return { factory, token, deployer, creator }; //this line is to export variables
     }   //to use its return var we must use line below
-        // const { factory } = await loadFixture(deployFactoryFixture)
+        // const { factory, deployer, creator } = await loadFixture(deployFactoryFixture)
 
     describe("Deployment", function() {
         it("Should have immutabe value", async() => {
@@ -28,10 +34,35 @@ describe("Factory", function () {
         it("Should have Same Owner", async() => {
             const { factory, deployer } = await loadFixture(deployFactoryFixture);
             expect(await factory.owner()).to.equal(deployer.address.toString());
+        });        
+    });
+
+    describe("Creating",function (params) {
+        
+        it("Should Set the Owner", async ()=> {
+            const { factory, token } = await loadFixture(deployFactoryFixture);
+            expect(await token.owner() ).to.equal(await factory.getAddress());
+        });
+
+        it("Should Set the creator", async ()=> {
+            const { creator, token } = await loadFixture(deployFactoryFixture);
+            expect(await token.creator() ).to.equal(await creator.getAddress());
+        });
+
+        it("Should Set the Supply", async ()=> {
+            const { factory, token } = await loadFixture(deployFactoryFixture);
+            const totalSupply = ethers.parseUnits("1000000",18); // 1000 tokens with 18 decimals
+            expect(await token.balanceOf(await factory.getAddress()) ).to.equal(totalSupply);
+        });
+
+        it("Should Update the Eth balance", async ()=> {
+            const { factory, token } = await loadFixture(deployFactoryFixture);
+            const balance = await ethers.provider.getBalance(await factory.getAddress());
+            expect(balance).to.equal(Fee);
         });
     });
 
-})
+});
 
 
 // Basic tst 
