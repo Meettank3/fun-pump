53c4674ed50a94cc45baaea38430fbaf76a77cf9
Meettank3 <125813530+Meettank3@users.noreply.github.com>
2025-09-18 00:42:06 +0530

    List Ui done

--- app/components/List.js
+++ app/components/List.js
@@ -1,10 +1,23 @@
-import { ethers } from "ethers"
+import { ethers } from "ethers";
 
 function List({ toggleCreate, fee, provider, factory }) {
 
   return (
-    <div className="list">
+    <div className = "list">
 
+      <h2> list new </h2>
+
+      <div className="list__description">
+        <p> Fee: {ethers.formatUnits(fee, 18) } ETH </p>
+      </div>
+
+      <form >
+        <input type="text" name="name" placeholder="name" />
+        <input type="text" name="ticker" placeholder="ticker" />
+        <input type="submit" value= "[ list ]" />
+      </form>
+
+      <button onClick={toggleCreate} className="btn--fancy" > [cancel] </button>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -8,13 +8,18 @@ import Header from "./components/Header";
 
 // ABIs & Config
 import Factory from "./abis/Factory.json";
+import List from "./components/List";
 
 export default function Home() {
   const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
   const [factory, setFactory] = useState(null);
   const [fee, setFee] = useState(0);
+  const [showCreate, setShowCreate] = useState(null);
 
+  function toggleCreate() {
+    showCreate ? setShowCreate(false) : setShowCreate(true);
+  }
 
   async function loadBlockchainData() {
     //if (typeof window === "undefined") return; // ✅ prevent SSR mismatch
@@ -38,6 +43,27 @@ export default function Home() {
     <div className="page">
       
       <Header account = {account} setAccount = {setAccount} />
+
+      <main>
+        <div className="create">
+          <button onClick={ factory && account && toggleCreate} className="btn--fancy">
+          { !factory ? (
+              "[Contract not deployed]"
+            ) : !account ? (
+              "[Please connect]"
+            ) : (
+              "[Start a New Token]"
+          )}
+          </button>
+        </div>
+      </main>
+    {
+      showCreate && 
+      ( 
+        <List toggleCreate={toggleCreate}  fee={fee} provider={provider} factory={factory} /> 
+      )
+    }
+      
 
     </div>
   );
