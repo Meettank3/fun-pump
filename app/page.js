65dd40f93cd6377e28f5bf947741a005f9e82ebd
Meettank3 <125813530+Meettank3@users.noreply.github.com>
2025-09-17 23:54:04 +0530

    Deployed coded and fee, abi etc...

--- app/page.js
+++ app/page.js
@@ -7,10 +7,13 @@ import config from "./config.json";
 import Header from "./components/Header";
 
 // ABIs & Config
+import Factory from "./abis/Factory.json";
 
 export default function Home() {
   const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
+  const [factory, setFactory] = useState(null);
+  const [fee, setFee] = useState(0);
 
 
   async function loadBlockchainData() {
@@ -19,7 +22,12 @@ export default function Home() {
     setProvider(provider);
 
     const network = await provider.getNetwork();    
-    console.log(config[network.chainId].factory.address);    
+
+    const factory = new ethers.Contract(config[network.chainId].factory.address, Factory, provider);
+    setFactory(factory);
+
+    const fee = await factory.fee();
+    setFee(fee);
   }
 
   useEffect( () => {
